c16e25070f055f20f8409a782fa937c3d5637cf2
salam-learn <143689270+salam-learn@users.noreply.github.com>
2023-09-20 11:07:15 +0300

    animations - Javascript

---
+++ js1/script.js
@@ -0,0 +1,26 @@
+let user = 'amer';
+
+console.log(user);
+
+let x = 9;
+
+console.log(Math.sqrt(x));
+
+console.log(Math.PI);
+
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+y = 9;
+console.log(y);
+
+let q  = [2,4,455];
+for(let c in q){
+    console.log(q[c]);
+}
+
+let t = document.getElementById("box");
+console.log(t);
+t.innerHTML = 'Finish';
+t.style.color = 'red';
